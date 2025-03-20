faa9ca0d99725602d6ab2c891cc9ec3e8ff4e783
BamSongE <soog2530@naver.com>
2025-03-20 16:06:26 +0900

    init

---
+++ carvery/src/main/java/com/example/carvery/CarveryApplication.java
@@ -0,0 +1,13 @@
+package com.example.carvery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarveryApplication.class, args);
+	}
+
+}
---
+++ carvery/src/test/java/com/example/carvery/CarveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.carvery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
